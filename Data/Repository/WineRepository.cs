d817fe761dc142a29a08ca41ee53e8c29fcc9dba
ReepoMeepo <jgzmaster@gmail.com>
2024-09-24 17:12:52 -0300

    Add project files.

---
+++ Data/Repository/WineRepository.cs
@@ -0,0 +1,52 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class WineRepository
+    {
+        public List<Wine> Wines { get; set; }
+        public WineRepository()
+        {
+            Wines = new List<Wine>()
+            {
+                new Wine()
+                {
+                    Id = 1,
+                    Name = "Pedrito",
+                    Variety = "mail@mail.com",
+                    Year = 1234,
+                    Region = "password"
+                },
+                new Wine()
+                {
+                    Id = 1,
+                    Name = "Pedrito",
+                    Variety = "mail@mail.com",
+                    Year = 1234,
+                    Region = "password"
+                },
+                new Wine()
+                {
+                    Id = 1,
+                    Name = "Pedrito",
+                    Variety = "mail@mail.com",
+                    Year = 1234,
+                    Region = "password"
+                },
+                new Wine()
+                {
+                    Id = 1,
+                    Name = "Pedrito",
+                    Variety = "mail@mail.com",
+                    Year = 1234,
+                    Region = "password"
+                }
+            };
+        }
+    }
+}
